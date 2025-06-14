a071d56c6decd3cc5cdf9b76569c8f9c802a43dc
user <user@DESKTOP-QK8T16E>
2025-06-15 08:10:01 +0900

    pyproject.tomlを作成

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-from setuptools import setup, Extension,find_packages # type: ignore
+from setuptools import setup, find_packages
 
 def get_requirements_from_file():
-    with open("./requirements.txt") as f_in:
-        requirements = f_in.read().splitlines()
-    return requirements
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
 
 setup(
-    name = 'ai_chat_lib',
-    version = '0.1.0',
-    author= 'knd3dayo',
-    author_email = 'knd3dayo@gmail.com',
-    packages=find_packages(where='.'),
-    package_dir={'ai_chat_lib': 'ai_chat_lib'},
+    name="ai_chat_lib",
+    version="0.1.0",
+    author="knd3dayo",
+    author_email="knd3dayo@gmail.com",
+    packages=find_packages(),
     install_requires=get_requirements_from_file(),
 )
